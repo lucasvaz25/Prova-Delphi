e168ed7a26cf027d10f607a772b0af5df014a5a5
unknown <lucas.vaz8@gmail.com>
2021-05-24 01:39:10 -0300

    complexo

--- 3 - complexo/QC2.dpr
+++ 3 - complexo/QC2.dpr
@@ -1,14 +1,15 @@
 program QC2;
 
 uses
-  uComponente in 'uComponente.pas',
-  uFrame in 'uFrame.pas',
-  uBotao in 'uBotao.pas',
-  uMemo in 'uMemo.pas',
-  uLabel in 'uLabel.pas';
+  UComponente in 'uComponente.pas',
+  UFrame in 'uFrame.pas',
+  UBotao in 'uBotao.pas',
+  UMemo in 'uMemo.pas',
+  ULabel in 'uLabel.pas';
 
 {$R *.RES}
 
-begin
-end.
 
+begin
+
+end.
